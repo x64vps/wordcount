2f359d801a6e27d0220ffc51f9a67afaa469ca4b
geralt <geralt@fixmost.com>
2021-08-22 14:11:52 +0300

    fix empty string bug

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"log"
 	"os"
 	"strings"
 )
@@ -12,17 +9,12 @@ import (
 func main()  {
 	argsWithProg := os.Args[1:]
 
+	if argsWithProg[0] == "" {
+		fmt.Println(0)
+		return
+	}
+
 	parts := strings.Split(argsWithProg[0], " ")
 
 	fmt.Println(len(parts))
-}
-
-// readString считывает и возвращает строку из os.Stdin
-func readString() string {
-	rdr := bufio.NewReader(os.Stdin)
-	str, err := rdr.ReadString('\n')
-	if err != nil && err != io.EOF {
-		log.Fatal(err)
-	}
-	return str
 }
